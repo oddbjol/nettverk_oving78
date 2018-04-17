66f47ec3daeb3511206a20e18a367cc3d0848167
oddbjol <olsen.oddbjorn@gmail.com>
2018-04-17 14:57:17 +0200

    stuff

--- threaded_server.py
+++ threaded_server.py
@@ -1,40 +1,31 @@
 import cv2
-from flask import Flask, render_template
+from flask import Flask
 import socketio
-import time
-from threading import Thread
 import base64
 
-from gevent import monkey
-monkey.patch_all()
+# Might need this...
+# from gevent import monkey
+# monkey.patch_all()
 
 sio = socketio.Server()
 app = Flask(__name__, static_url_path='/static')
 app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
 thread = None
 
-sid2 = 0
 @sio.on('connect')
 def connect(sid, environ):
-    global sid2, thread
-    sid2 = sid
+    global thread
 
     if thread is None:
         thread = sio.start_background_task(video_thread)
 
     print("connect ", sid)
-    print("sio.emit('message', room=", sid, ")")
-
-
-@sio.on('message')
-async def message(sid, data):
-    print("message ", data)
-    await sio.emit('message', "reply to message", room=sid)
 
 
 @sio.on('disconnect')
 def disconnect(sid):
     print('disconnect ', sid)
+
 
 def video_thread():
 
@@ -48,7 +39,7 @@ def video_thread():
 
     while True:
 
-        sio.sleep(1/60)
+        sio.sleep(1/60)  # 60 fps
 
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -56,7 +47,6 @@ def video_thread():
         # Handles the mirroring of the current frame
         frame = cv2.flip(frame, 1)
         # frame = cv2.resize(frame, (200,200))
-
 
         retval, buffer = cv2.imencode('.jpg', frame)
         jpg_as_text = base64.b64encode(buffer)
@@ -71,7 +61,7 @@ def video_thread():
         # Display the resulting frame
         # cv2.imshow('frame', frame)
 
-        print("in loop: sio.emit('message', room=", sid2, ")")
+        print("sending frame ", currentFrame)
         sio.emit('message', str(jpg_as_text))
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -86,6 +76,7 @@ def video_thread():
 
 if __name__ == '__main__':
 
+    print("starting server.")
     # deploy with gevent
     from gevent import pywsgi
     try:
@@ -94,9 +85,9 @@ if __name__ == '__main__':
     except ImportError:
         websocket = False
     if websocket:
-        pywsgi.WSGIServer(('', 8080), app, handler_class=WebSocketHandler).serve_forever()
+        pywsgi.WSGIServer(('', 8080), app, handler_class=WebSocketHandler, log=None).serve_forever()
     else:
-        pywsgi.WSGIServer(('', 8080), app).serve_forever()
+        pywsgi.WSGIServer(('', 8080), app, log=None).serve_forever()
 
 
 
